2696aefe6589330b35d879289d6edc52e8354d13
lini07003 <lini07003@E-PW05C37N>
2024-09-11 12:16:09 +0200

    Add project files.

---
+++ inlämningsUpg1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Upgift1_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Detta kommer vara en text//
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            //nästa kommand kommer vara en till text//
+            Console.WriteLine("Du har lärt dig att skriva ut tecken som \" och /");
+        }
+    }
+}
